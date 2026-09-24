1b46c16d43405bced81dc4226513aae99d99ae54
Palash Pingale <palashpingale135@gmail.com>
2026-09-24 23:18:21 +0530

    Update langgraph_backend.py

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -21,7 +21,6 @@ def chat_node(state: ChatState):
 # Checkpointer
 checkpointer = InMemorySaver()
 
-
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
